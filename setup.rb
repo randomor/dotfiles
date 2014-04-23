7538c54675a8f034a192b1aede6f6b47f93c7bab
Shaomeng Zhang <shao@x-134-84-56-176.uofm-secure.wireless.umn.edu>
2014-04-23 17:10:43 -0500

    Added sublime

--- setup.rb
+++ setup.rb
@@ -4,6 +4,8 @@ PWD = File.dirname(__FILE__)
 #Install HomeBrew
 #`ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"`
 
+# system("brew bundle #{PWD}/Brewfile")
+
 #Install Oh-my-zsh
 #`curl -L http://install.ohmyz.sh | sh`
 # `ln -s ~/.dotfiles/zsh ~/.oh-my-zsh`
@@ -14,3 +16,6 @@ system("ln -s #{PWD}/zsh/themes/aMetheme.zsh-theme ~/.oh-my-zsh/themes/aMetheme.
 #Link .zshrc
 # `mv ~/.zshrc ~/.zshrc.old`
 # system "ln -s #{PWD}/.zshrc ~/.zshrc"
+
+#Create sublime text link
+system("ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl")
